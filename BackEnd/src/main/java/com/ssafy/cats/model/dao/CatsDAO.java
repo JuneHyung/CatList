977ca49cb09e99cfb81ac277472cb7c4cdb69f06
조준형 <cjh951114@naver.com>
2021-03-29 20:16:48 +0900

    Create CatsDAO.java
    cats.xml과 연결하여 실행할 DAO

---
+++ BackEnd/src/main/java/com/ssafy/cats/model/dao/CatsDAO.java
@@ -0,0 +1,13 @@
+package com.ssafy.cats.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.cats.model.Cats;
+
+@Mapper
+public interface CatsDAO {
+	public List<Cats> listCats(int start);
+//	public Cats detailCats(int cat_num);
+}
