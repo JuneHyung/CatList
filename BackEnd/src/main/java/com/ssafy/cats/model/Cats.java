4ae6fd10d0d576e4faa2dddeb6a37055155a1ee0
조준형 <cjh951114@naver.com>
2021-03-29 20:16:18 +0900

    Create Cats.java
    Cats의 정보를 담을 DTO

---
+++ BackEnd/src/main/java/com/ssafy/cats/model/Cats.java
@@ -0,0 +1,48 @@
+package com.ssafy.cats.model;
+
+public class Cats {
+	String cat_num;
+	String cat_name;
+	String create_date;
+	String kind;
+	String profile;
+	String description;
+	public String getCat_num() {
+		return cat_num;
+	}
+	public void setCat_num(String cat_num) {
+		this.cat_num = cat_num;
+	}
+	public String getCat_name() {
+		return cat_name;
+	}
+	public void setCat_name(String cat_name) {
+		this.cat_name = cat_name;
+	}
+	public String getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
