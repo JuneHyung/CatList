8656cab5267f9dc6d6602c35547465db71be3758
조준형 <cjh951114@naver.com>
2021-03-29 20:17:17 +0900

    create CatsServiceImpl.java
    create CatsServiceImpl.java

---
+++ BackEnd/src/main/java/com/ssafy/cats/model/service/CatsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ssafy.cats.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.cats.model.Cats;
+import com.ssafy.cats.model.dao.CatsDAO;
+
+@Service
+public class CatsServiceImpl implements CatsService{
+
+	@Autowired
+	private CatsDAO catsDao;
+	
+	@Override
+	public List<Cats> listCats(int start) {
+		// TODO Auto-generated method stub
+		return catsDao.listCats(start);
+	}
+
+//	@Override
+//	public Cats detailCats(int cat_num) {
+//		// TODO Auto-generated method stub
+//		return catsDao.detailCats(cat_num);
+//	}
+
+}
