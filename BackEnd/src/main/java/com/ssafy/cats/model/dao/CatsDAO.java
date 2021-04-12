9211c653451a133cb33491d488f277a31e338d20
조준형 <cjh951114@naver.com>
2021-04-12 18:06:29 +0900

    update DAO : add Searching
    update DAO : add Searching

--- BackEnd/src/main/java/com/ssafy/cats/model/dao/CatsDAO.java
+++ BackEnd/src/main/java/com/ssafy/cats/model/dao/CatsDAO.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+
 import com.ssafy.cats.model.Cats;
 
 @Mapper
 public interface CatsDAO {
 	public List<Cats> listCats(int start);
 	public Cats detailCats(int cat_num);
+	public List<Cats> searchCats(String keyword);
 }
