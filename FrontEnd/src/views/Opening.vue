f808a719c0c1e4f85bdb6a273bec6f86357c9951
조준형 <cjh951114@naver.com>
2021-04-14 21:01:33 +0900

    Create Opening.vue
    Create Opening.vue
    오프닝페이지

---
+++ FrontEnd/src/views/Opening.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-container class="bgBox">
+        <div class="titleBox">
+            <h1 class="openingTitle">Let's Go</h1>
+            <p @click="goMain()">Search Cats</p>
+        </div>
+    </v-container>
+</template>
+<script>
+export default {
+    methods: {
+        goMain() {
+            this.$router.push({ name: 'Main' });
+        },
+    },
+};
+</script>
+<style scoped>
+.bgBox {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background: url('../assets/images/openbg.jpeg');
+    background-size: 100% 100%;
+    background-position: center;
+}
+.titleBox {
+    width: 500px;
+    height: 300px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -150px;
+    margin-left: -250px;
+}
+.openingTitle {
+    width: 100%;
+    text-align: center;
+    font-size: 120px;
+}
+.titleBox p {
+    font-size: 60px;
+    text-align: center;
+    cursor: pointer;
+}
+</style>
