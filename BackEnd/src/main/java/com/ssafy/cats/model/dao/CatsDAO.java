f5301cda2837c91e7750fb911db7a6c133806528
조준형 <cjh951114@naver.com>
2021-04-18 16:30:25 +0900

    Update CatsDAO : 고양이 성격정보 가져오기
    Update CatsDAO : 고양이 성격정보 가져오기

--- BackEnd/src/main/java/com/ssafy/cats/model/dao/CatsDAO.java
+++ BackEnd/src/main/java/com/ssafy/cats/model/dao/CatsDAO.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-
 import com.ssafy.cats.model.Cats;
+import com.ssafy.cats.model.Charc;
 
 @Mapper
 public interface CatsDAO {
 	public List<Cats> listCats(int start);
 	public Cats detailCats(int cat_num);
 	public List<Cats> searchCats(String keyword);
+	public Charc charcCats(int cat_num);
 }
