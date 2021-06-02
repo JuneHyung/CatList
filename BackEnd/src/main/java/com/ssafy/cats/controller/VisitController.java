02a152d9e0856494eff8846f8e60408fb00a039c
JuneHyung <cjh951114@gmail.com>
2021-06-02 16:00:43 +0900

    Create VisitController
    Create VisitController

---
+++ BackEnd/src/main/java/com/ssafy/cats/controller/VisitController.java
@@ -0,0 +1,31 @@
+package com.ssafy.cats.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.cats.model.Visits;
+import com.ssafy.cats.model.service.VisitsService;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/visits")
+public class VisitController {
+
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+	
+	@Autowired
+	private VisitsService visitService;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Visits>> listVisits() throws Exception{
+		return new ResponseEntity<List<Visits>>(visitService.listVisits(), HttpStatus.OK);
+	}
+}
