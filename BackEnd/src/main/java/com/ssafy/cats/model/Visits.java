759668b81734339f462cb2fb0c00b5eff204f199
JuneHyung <cjh951114@gmail.com>
2021-06-02 16:01:15 +0900

    Create Visits DTO
    Create Visits DTO

---
+++ BackEnd/src/main/java/com/ssafy/cats/model/Visits.java
@@ -0,0 +1,27 @@
+package com.ssafy.cats.model;
+
+public class Visits {
+	int visit_id;
+	String today;
+	int views;
+	public int getVisit_id() {
+		return visit_id;
+	}
+	public void setVisit_id(int visit_id) {
+		this.visit_id = visit_id;
+	}
+	public String getToday() {
+		return today;
+	}
+	public void setToday(String today) {
+		this.today = today;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+	
+	
+}
