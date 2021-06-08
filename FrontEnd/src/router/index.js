e810f84f6637552f91d9a6c3e5c5a18494b7ca96
JuneHyung <cjh951114@gmail.com>
2021-06-08 18:10:00 +0900

    v2.2 : 라우터 중복버그 수정
    같은 url의 링크를 타게되면 버그발생.하여 수정.

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -5,6 +5,14 @@ import Opening from '../views/Opening.vue';
 import Main from '../views/Main.vue';
 import Visit from '../views/Visit.vue';
 
+/** 같은 페이지에서 같은 페이지로 $router.push 한 오률를 처리함 (ex : 홈페이지에서 홈 로고를 클릭한 경우) */
+const originalPush = VueRouter.prototype.push;
+  VueRouter.prototype.push = function push(location){
+  return originalPush.call(this, location).catch(()=>{
+  return window.location.reload()
+  })
+};
+
 Vue.use(VueRouter);
 export default new VueRouter({
   mode: 'history',
