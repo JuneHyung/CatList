99eeada0fea5a4c5a52f20e89368a7e7040a2d84
JUNEHYUNG\USER <cjh951114@gmail.com>
2023-08-16 17:17:11 +0900

    feat: intro-page 수정 및 메인 background Color 설정

--- FrontEnd/src/pages/IntroPage.jsx
+++ FrontEnd/src/pages/IntroPage.jsx
@@ -2,13 +2,12 @@ import { Link } from "react-router-dom";
 
 const IntroPage = () => {
   return (
-    <div className="intro-page blinking">
-      <div className="coverBox card-demo">
-        <Link id="step1" className="titleBox" to={"catList"}>
-          <h1 className="openingTitle card-demo-link">Let's Go</h1>
-          <p id="step2">Search Cats</p>
+    <div className="intro-page">
+      <div className="intro-wrap-box">
+        <Link id="step1" className="intro-logo" to={"catList"}>
+            <img src={require('./../assets/images/intro-logo.png')} alt='logo'></img>
         </Link>
-      </div>
+        </div>
     </div>
   );
 };
