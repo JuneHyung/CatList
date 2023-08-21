2a8294825b9321df866e7a45a829046a08ef25d0
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-08-21 11:33:01 +0900

    feat : cat Kind image 추가

--- FrontEnd/src/componenets/catList/CatKindItem.jsx
+++ FrontEnd/src/componenets/catList/CatKindItem.jsx
@@ -6,16 +6,14 @@ import { useCallback } from "react";
 const CatKindItem = ({item}) =>{
   const {selectedKind} = useSelector((state)=> state.cat)
   const dispatch = useDispatch();
-  // const handleOnClick = useCallback(() =>{
-  //   dispatch(setSelectedKind(item.kind_name));
-  // }, [item.kind, dispatch])
+  
   const handleOnClick = useCallback(() =>{
-    // dispatch(setSelectedKind(item.kind_name));
-  }, [])
+    dispatch(setSelectedKind(item.kind_name));
+  }, [item.kind_name, dispatch])
 
   return (
     <li className={`cat-kind-item ${selectedKind === item.kind_name ? 'active-kind' : ''}`} onClick={handleOnClick}>
-      <Avatar url={item.profile} />
+      <Avatar url={item.kind_profile} />
       {item.kind_name}
     </li>
   )
