d65c7ac2a7f004e9aef694870027ef26b36447d7
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-08-27 23:46:24 +0900

    feat : badgeItem 조회 api 연결

--- FrontEnd/src/componenets/catList/BadgeItem.jsx
+++ FrontEnd/src/componenets/catList/BadgeItem.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getCatListByKeyword } from "../../stores/actions/cat";
 
 const BadgeItem = ({item}) =>{
   const bgNameList =  [
@@ -20,8 +22,16 @@ const BadgeItem = ({item}) =>{
     'nearMoon',
   ];
   const [bgName, setBgName] = useState( `bg-${bgNameList[Math.floor(Math.random() * bgNameList.length)]}`);
+
+  const dispatch = useDispatch();
+  
+  const handleOnClick = useCallback(() =>{
+    dispatch(getCatListByKeyword({keyword:item, curPage: 1}));
+  }, [dispatch, item])
+
+
   return(
-    <li className={`badge-item jh-mr-sm ${bgName}`}>{item}</li>
+    <li className={`badge-item jh-mr-sm ${bgName}`} onClick={handleOnClick}>{item}</li>
   )
 }
 export default BadgeItem;
