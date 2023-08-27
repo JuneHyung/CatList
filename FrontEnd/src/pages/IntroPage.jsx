7e8df6aa23f11fa1c4b53cd916b931dbec3572ee
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-08-27 23:10:50 +0900

    fix : intropage css 수정

--- FrontEnd/src/pages/IntroPage.jsx
+++ FrontEnd/src/pages/IntroPage.jsx
@@ -2,13 +2,11 @@ import { Link } from "react-router-dom";
 
 const IntroPage = () => {
   return (
-    <div className="intro-page">
       <div className="intro-wrap-box">
         <Link id="step1" className="intro-logo" to={"catList"}>
             <img src={require('./../assets/images/intro-logo.png')} alt='logo'></img>
         </Link>
-        </div>
-    </div>
+      </div>
   );
 };
 
