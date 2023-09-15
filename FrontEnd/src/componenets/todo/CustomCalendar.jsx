33ad24d0319a53976651eaab1f403a9f419cebe1
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-09-15 19:25:47 +0900

    feat : calendar 옵션 추가 및 테스트

--- FrontEnd/src/componenets/todo/CustomCalendar.jsx
+++ FrontEnd/src/componenets/todo/CustomCalendar.jsx
@@ -1,10 +1,36 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import { useCallback, useEffect, useState } from 'react'
 
 const CustomCalendar = () => {
+  const [events, setEvents] = useState([])
+
+  useEffect(()=>{
+    setEvents([{title:'test00', date:'2023-09-15'},{title:'test01', date:'2023-09-15'},{title:'test01', date:'2023-09-15'},{title:'test01', date:'2023-09-15'},{title:'test02', date:'2023-09-16'}]);
+  }, [])
+
+  const handleOnClick = useCallback(()=>{
+    setEvents([{title:'test00', date:'2023-09-15'},{title:'test01', date:'2023-09-17'},{title:'test01', date:'2023-09-17'},{title:'test01', date:'2023-09-17'},{title:'test02', date:'2023-09-17'}]);
+  }, [])
+  
+
   return (
-    <FullCalendar plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"/>
+    <>
+    <FullCalendar 
+      selectable
+      plugins={[dayGridPlugin]}
+      headerToolbar={{
+        left: 'prevYear prev',
+        center: 'title',
+        right:'next nextYear',
+      }}
+      initialView="dayGridMonth"
+      events= {events}
+      dayMaxEvents={3}
+      contentHeight={500}
+    />
+    <button onClick={handleOnClick}>새 data init test</button>
+    </>
   )
 }
 
