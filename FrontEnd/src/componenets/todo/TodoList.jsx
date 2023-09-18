f391ce84a1fab6ff520bd8f4e047e6068874994e
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-09-18 11:30:25 +0900

    feat : todo, doing, done tab click event 추가

--- FrontEnd/src/componenets/todo/TodoList.jsx
+++ FrontEnd/src/componenets/todo/TodoList.jsx
@@ -1,15 +1,21 @@
-import { useSelector } from "react-redux";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setTodoList } from "../../stores/actions/todo";
 
 const TodoList = ({today}) => {
   const {todoList} = useSelector((state)=>state.todo);
+  const dispatch = useDispatch();
+  const handleGetTodo = useCallback((status)=>{
+    dispatch(setTodoList(status))
+  },[dispatch])
   return (
     <div>
       <ul className="todo-tab-bar">
         <li> {today}</li>
         <ul className="todo-active-tab-bar">
-          <li className="pointer-cursor">TODO</li>
-          <li className="pointer-cursor">DOING</li>
-          <li className="pointer-cursor">DONE</li>
+          <li className="pointer-cursor" onClick={()=>handleGetTodo('todo')}>TODO</li>
+          <li className="pointer-cursor" onClick={()=>handleGetTodo('doing')}>DOING</li>
+          <li className="pointer-cursor" onClick={()=>handleGetTodo('done')}>DONE</li>
         </ul>
       </ul>
       <div className="plus-item-button pointer-cursor">PLUS ITEM</div>
