e9af8494b19d89a4a668e7121b84b10089e37464
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-11-28 16:55:00 +0900

    feat: Menubar 수정

--- FrontEnd/src/componenets/tiptap/EditorMenuBar.tsx
+++ FrontEnd/src/componenets/tiptap/EditorMenuBar.tsx
@@ -1,112 +1,73 @@
+import {useCallback} from 'react';
+import MdiIcon from "../common/MdiIcon";
+
 const EditorMenuBar = ({ editor }) => {
-  if (!editor) {
-    return null
-  }
+  
+  const handleFormatHeader = useCallback((level: number)=>{
+    editor.chain().focus().toggleHeading({level}).run();
+  }, [editor])
+  const handleFormatBold = useCallback(()=>{
+    editor.chain().focus().toggleBold().run();
+  }, [editor])
+  const handleFormatItalic = useCallback(()=>{
+    editor.chain().focus().toggleItalic().run();
+  }, [editor])
+  const handleFormatStrikethrough = useCallback(()=>{
+    editor.chain().focus().toggleStrike().run();
+  }, [editor])
+  const handleFormatQuoteOpen = useCallback(()=>{
+    editor.chain().focus().toggleBlockquote().run();
+  }, [editor])
+  const handleFormatLink = useCallback(()=>{
+    if (editor.isActive("link")) {
+      editor.chain().focus().unsetLink().run();
+      return;
+    }
+    const previousUrl = editor.getAttributes("link").href;
+    const url = window.prompt("URL", previousUrl);
+
+    // cancelled
+    if (url === null) {
+      return;
+    }
+
+    // empty
+    if (url === "") {
+      editor.chain().focus().extendMarkRange("link").unsetLink().run();
+      return;
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange("link").setLink({ href: url }).run();
+  }, [editor])
+  const handleFormatImage = useCallback(()=>{
+    console.log('보류')
+  }, [])
+  const handleFormatCodTags = useCallback(()=>{
+    editor.chain().focus().toggleCode().run();
+  }, [editor])
+  
 
   return (
-    <>
-    <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
-      >
-        h1
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
-      >
-        h2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-      >
-        h3
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
-      >
-        h4
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleBold()
-            .run()
-        }
-        className={editor.isActive('bold') ? 'is-active' : ''}
-      >
-        bold
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleItalic()
-            .run()
-        }
-        className={editor.isActive('italic') ? 'is-active' : ''}
-      >
-        italic
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleStrike()
-            .run()
-        }
-        className={editor.isActive('strike') ? 'is-active' : ''}
-      >
-        strike
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleCode()
-            .run()
-        }
-        className={editor.isActive('code') ? 'is-active' : ''}
-      >
-        emphasis
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        bullet list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        ordered list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
-      >
-        code block
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? 'is-active' : ''}
-      >
-        blockquote
-      </button>
-      
-    </>
+    <ul className="editor-menu-group-wrap">
+      <li className="editor-menu-group">
+        <button className="editor-menu-button" onClick={()=>handleFormatHeader(1)}><MdiIcon name="mdiFormatHeader1"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={()=>handleFormatHeader(2)}><MdiIcon name="mdiFormatHeader2"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={()=>handleFormatHeader(3)}><MdiIcon name="mdiFormatHeader3"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={()=>handleFormatHeader(4)}><MdiIcon name="mdiFormatHeader4"></MdiIcon> </button>
+      </li>
+      <li className="editor-menu-group">
+        <button className="editor-menu-button" onClick={handleFormatBold}><MdiIcon name="mdiFormatBold"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={handleFormatItalic}><MdiIcon name="mdiFormatItalic"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={handleFormatStrikethrough}><MdiIcon name="mdiFormatStrikethrough"></MdiIcon> </button>
+      </li>
+      <li className="editor-menu-group">
+        <button className="editor-menu-button" onClick={handleFormatQuoteOpen}><MdiIcon name="mdiFormatQuoteOpen"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={handleFormatLink}><MdiIcon name="mdiLink"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={handleFormatImage}><MdiIcon name="mdiImage"></MdiIcon> </button>
+        <button className="editor-menu-button" onClick={handleFormatCodTags}><MdiIcon name="mdiCodeTags"></MdiIcon> </button>
+      </li>
+    </ul>
   )
 }
 export default EditorMenuBar;
